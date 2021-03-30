0f4322b8671705e71d894686bbae4808782bc14e
Lexnick <70635054+Lexnick@users.noreply.github.com>
2021-03-30 23:08:39 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -16,18 +17,28 @@ struct Citizen {
     int Age;
 };
 
+bool operator==(const Citizen &S1, const Citizen &S2) {
+    return (S1.FullName == S2.FullName);
+}
+
+bool operator!=(const Citizen &S1, const Citizen &S2) {
+    return !(S1 == S2);
+}
+
+bool operator>(const Citizen &S1, const Citizen &S2) {
+    return (S1.FullName > S2.FullName);
+}
+
+bool operator<(const Citizen &S1, const Citizen &S2) {
+    return (S1.FullName < S2.FullName);
+}
+
 void FullNameSort(std::vector<Citizen> &vec) {
-    std::set<std::string> SetBase;
+    std::set<Citizen> SetBase;
     for (auto &VecElement: vec) {
-        SetBase.insert(VecElement.FullName);
+        SetBase.insert(VecElement);
     }
-    auto iter = SetBase.begin();
-    for (int i = 0; i < vec.size(); i++) {
-        for (int j = i + 1; j < vec.size(); j++) {
-            if (*iter == vec[j].FullName) std::swap(vec[i], vec[j]);
-        }
-        ++iter;
-    }
+    std::transform(SetBase.begin(), SetBase.end(), vec.begin(), [](const Citizen &Element) { return Element; });
 }
 
 
@@ -37,18 +48,19 @@ void OutputTextBinary(std::vector<Citizen> &vec) {
     outputFile.write(reinterpret_cast<char *>(&Size), sizeof(Size));
     for (auto &BaseElement: vec) {
 
-        int NameSize = BaseElement.FullName.size() + 1;
+        int NameSize = BaseElement.FullName.size()+1;
         outputFile.write(reinterpret_cast<char *>(&NameSize), sizeof(NameSize));
-        int StreetSize = BaseElement.CitizenAddress.Street.size() + 1;
+        int StreetSize = BaseElement.CitizenAddress.Street.size()+1;
         outputFile.write(reinterpret_cast<char *>(&StreetSize), sizeof(StreetSize));
-        int SexSize = BaseElement.Sex.size() + 1;
+        int SexSize = BaseElement.Sex.size()+1;
         outputFile.write(reinterpret_cast<char *>(&SexSize), sizeof(SexSize));
 
-        outputFile.write(reinterpret_cast<char *>(&BaseElement.FullName), NameSize);
+        outputFile.write(BaseElement.FullName.c_str(), NameSize);
 
-        outputFile.write(reinterpret_cast<char *>(&BaseElement.CitizenAddress.Street), StreetSize);
+        outputFile.write(BaseElement.CitizenAddress.Street.c_str(),
+                         StreetSize);
 
-        outputFile.write(reinterpret_cast<char *>(&BaseElement.Sex), SexSize);
+        outputFile.write(BaseElement.Sex.c_str(),SexSize);
 
         int HouseNumber = BaseElement.CitizenAddress.HouseNumber;
         outputFile.write(reinterpret_cast<char *>(&HouseNumber), sizeof(HouseNumber));
@@ -69,7 +81,7 @@ void InputTextBinary(std::vector<Citizen> &vec) {
     int Size = 0;
     inputFile.read(reinterpret_cast<char *>(&Size), sizeof(Size));
     for (int i = 0; i < Size; i++) {
-        Citizen Person;
+        Citizen Element;
 
         int NameSize = 0;
         inputFile.read(reinterpret_cast<char *>(&NameSize), sizeof(NameSize));
@@ -78,42 +90,37 @@ void InputTextBinary(std::vector<Citizen> &vec) {
         int SexSize = 0;
         inputFile.read(reinterpret_cast<char *>(&SexSize), sizeof(SexSize));
 
-        char *FullName = new char[NameSize];
-        inputFile.read(reinterpret_cast<char *>(&FullName), NameSize);
-        std::string PersonFullName(FullName);
-        Person.FullName = PersonFullName;
+        char* FullName=new char[NameSize];
+        inputFile.read(FullName, NameSize);
+        Element.FullName=FullName;
+        delete [] FullName;
 
-        char *Street = new char[StreetSize];
-        inputFile.read(reinterpret_cast<char *>(&Street), StreetSize);
-        std::string PersonStreet(Street);
-        Person.CitizenAddress.Street = PersonStreet;
+        char* Street=new char[StreetSize];
+        inputFile.read(Street, StreetSize);;
+        Element.CitizenAddress.Street=Street;
+        delete [] Street;
 
-        char *Sex = new char[SexSize];
-        inputFile.read(reinterpret_cast<char *>(&Sex), SexSize);
-        std::string ThisSex(Sex);
-        Person.Sex = ThisSex;
+        char* Sex=new char[SexSize];
+        inputFile.read(Sex, SexSize);
+        Element.Sex=Sex;
+        delete [] Sex;
 
-        int HouseNumber = 0;
-        inputFile.read(reinterpret_cast<char *>(&HouseNumber), sizeof(HouseNumber));
-        Person.CitizenAddress.HouseNumber = HouseNumber;
+        inputFile.read(reinterpret_cast<char *>(&Element.CitizenAddress.HouseNumber),
+                       sizeof(Element.CitizenAddress.HouseNumber));
 
-        int FlatNumber = 0;
-        inputFile.read(reinterpret_cast<char *>(&FlatNumber), sizeof(FlatNumber));
-        Person.CitizenAddress.FlatNumber = FlatNumber;
 
-        int Age = 0;
-        inputFile.read(reinterpret_cast<char *>(&Age), sizeof(Age));
-        Person.Age = Age;
-        vec.push_back(Person);
-        delete[]FullName;
-        delete[]Street;
-        delete[]Sex;
+        inputFile.read(reinterpret_cast<char *>(&Element.CitizenAddress.FlatNumber),
+                       sizeof(Element.CitizenAddress.FlatNumber));
+
+        inputFile.read(reinterpret_cast<char *>(&Element.Age), sizeof(Element.Age));
+        vec.push_back(Element);
     }
     inputFile.close();
 }
 
 void operator<<(const std::string &File, std::vector<Citizen> &vec) {
     std::ofstream outputFile(File);
+    outputFile << vec.size() << ' ';
     for (const auto &i: vec) {
         outputFile << i.FullName << '\n' << i.CitizenAddress.Street << ' ' << i.CitizenAddress.HouseNumber << ' '
                    << i.CitizenAddress.FlatNumber << ' ' << i.Age << ' ' << i.Sex << std::endl;
@@ -123,22 +130,27 @@ void operator<<(const std::string &File, std::vector<Citizen> &vec) {
 
 void operator>>(const std::string &File, std::vector<Citizen> &vec) {
     std::ifstream inputFile(File);
+    int Size = 0;
+    std::string str;
+    inputFile >> str;
+    Size = stoi(str);
     int i = 0;
-    while ((!inputFile.eof()) && (i < vec.size())) {
-        std::string str;
-        std::getline(inputFile, str);
-        vec[i].FullName = str;
-        inputFile >> str;
-        vec[i].CitizenAddress.Street = str;
-        inputFile >> str;
-        vec[i].CitizenAddress.HouseNumber = std::stoi(str);
-        inputFile >> str;
-        vec[i].CitizenAddress.FlatNumber = std::stoi(str);
-        inputFile >> str;
-        vec[i].Age = std::stoi(str);
-        inputFile >> str;
-        vec[i].Sex = str;
+    while ((!inputFile.eof()) && (i < Size)) {
+        Citizen Element;
         inputFile.ignore();
+        std::getline(inputFile, str);
+        Element.FullName = str;
+        inputFile >> str;
+        Element.CitizenAddress.Street = str;
+        inputFile >> str;
+        Element.CitizenAddress.HouseNumber = std::stoi(str);
+        inputFile >> str;
+        Element.CitizenAddress.FlatNumber = std::stoi(str);
+        inputFile >> str;
+        Element.Age = std::stoi(str);
+        inputFile >> str;
+        Element.Sex = str;
+        vec.push_back(Element);
         i++;
     }
     inputFile.close();
@@ -162,10 +174,10 @@ int main() {
              {"Bogomolova Alina Vladimirovna",   {"Rabochaya",   1,  128}, "Female", 60},
              {"Ryabova Eva Makarovna",           {"Kolcevaya",   1,  128}, "Female", 2}};
     FullNameSort(HMO);
-    for (auto & i : HMO) {
+    for (auto &i : HMO) {
         std::cout << i.FullName << std::endl;
     }
-    std::cout<<"-------------"<<std::endl;
+    std::cout << "-------------" << std::endl;
 
     std::cout << "Enter the street name" << std::endl;
     std::string InputStreet;
@@ -180,7 +192,7 @@ int main() {
         if ((i.CitizenAddress.Street == InputStreet) && (i.Age < 7))
             std::cout << i.FullName << std::endl;//printing value and list of objects
     }
-    std::cout<<"-------------"<<std::endl;
+    std::cout << "-------------" << std::endl;
 
     std::vector<Citizen> BaseBinaryTest;
     OutputTextBinary(HMO);
@@ -190,14 +202,13 @@ int main() {
                   << i.CitizenAddress.FlatNumber << ' ' << i.Age << ' ' << i.Sex << std::endl;
     }
     std::cout << "-------------" << std::endl;
-    std::vector<Citizen> BaseTextTest(HMO.size());
-    "HMO 123.txt"<<HMO;
-    "HMO 123.txt">>BaseTextTest;
+    std::vector<Citizen> BaseTextTest;
+    "HMO 123.txt" << HMO;
+    "HMO 123.txt" >> BaseTextTest;
     for (auto &i:BaseTextTest) {
         std::cout << i.FullName << ' ' << i.CitizenAddress.Street << ' ' << i.CitizenAddress.HouseNumber << ' '
                   << i.CitizenAddress.FlatNumber << ' ' << i.Age << ' ' << i.Sex << std::endl;
     }
-    std::cout<<"-------------"<<std::endl;
-    
+    std::cout << "-------------" << std::endl;
     return 0;
 }
